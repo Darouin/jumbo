9f2c6420a57d32362d50bc512ad8e2546db84065
Olivier <openas@ippon.fr>
2022-07-30 12:30:13 +0200

    init project

---
+++ webservices/src/test/java/com/jumbo/ArchTest.java
@@ -0,0 +1,29 @@
+package com.jumbo;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.jumbo");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.jumbo.service..")
+            .or()
+            .resideInAnyPackage("com.jumbo.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.jumbo.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
---
+++ webservices/src/test/java/com/jumbo/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,11 @@
+package com.jumbo.cucumber;
+
+import com.jumbo.WebservicesApp;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = WebservicesApp.class)
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
